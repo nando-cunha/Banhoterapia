66d208f6ece132f811bff503389d6358a87b4ea9
Fernando Cunha <fe.apcunha@gmail.com>
2021-03-03 14:21:58 -0300

    versionamento do meu primeiro site para Deploy

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Casa de banhos termais - BANHOTERAPIA - Aqui seu banho é levado a sério</title>
+</head>
+<body>
+
+<h1>BANHO TERAPIA - Casa de banhos termais.</h1>
+<p></p>
+</body>
+</html>
