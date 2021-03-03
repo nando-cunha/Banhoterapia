e30174c7cd6095f1e8bd153626bbc14dfdd1fede
Fernando Cunha <fe.apcunha@gmail.com>
2021-03-03 15:59:03 -0300

    Trabalhando com cores HTML

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Casa de banhos termais - BANHOTERAPIA - Aqui seu banho é levado a sério</title>
-</head>
-<body>
-
-<h1>BANHO TERAPIA - Casa de banhos termais.</h1>
-<p></p>
-</body>
+   <head>
+      <title>Casa de banhos termais - BANHOTERAPIA - Aqui seu banho é levado a sério</title>
+   </head>
+   <body>
+      <h1>BANHO TERAPIA - Casa de banhos termais.</h1>
+      <p></p>
+      <div style="width:100%;display:flex;height:80px">
+         <div style="width:50%;background:blue"></div>
+         <div style="width:50%;background:red"></div>
+   </body>
 </html>
